7967e78f16f76fa7f21acff3d5b3d90430e75f52
lucassanjuan <lusanjuan@poligran.edu.co>
2019-09-30 22:29:50 -0500

    Creacion de Slider
    Creamos el componente SLIDER con sus archivos relacionados

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component';
-import { ContainerComponent } from './components/container/container.component'
+import { ContainerComponent } from './components/container/container.component';
+import { SliderComponent } from './components/slider/slider.component'
 
 @NgModule({
-  declarations: [HomeComponent, ContainerComponent],
+  declarations: [HomeComponent, ContainerComponent, SliderComponent],
   imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule {}
