813193f587af0c61a96a64210a9caac3992db34f
Carol Mariana Velazquez <cavelasquez7@poligran.edu.co>
2019-09-30 22:09:25 -0500

    Home Component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
-import { HomeComponent } from './home.component'
+import { HomeComponent } from './home.component';
+import { ContainerComponent } from './components/container/container.component'
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ContainerComponent],
   imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule {}
