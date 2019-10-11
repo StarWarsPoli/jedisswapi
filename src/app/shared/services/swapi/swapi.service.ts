f49e8363425111eb765be2ea8066a365cdfc2ed9
Hugo Angulo <huangulo@poligran.edu.co>
2019-10-10 22:43:48 -0500

    Create GET AllFilms Service
    Se creo el servicio para consumir todas las peliculas

---
+++ src/app/shared/services/swapi/swapi.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { take } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SwapiService {
+
+  private urlBase = 'https://swapi.co/api/'
+
+  constructor(private http: HttpClient) { }
+
+  getAllFilms(): Observable<any> {
+    const url = `${this.urlBase}films`
+    return this.http.get<any>(url).pipe(take(1))
+  }
+}
