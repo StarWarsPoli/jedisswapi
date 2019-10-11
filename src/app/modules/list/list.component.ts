0926fa6a58936263132f4affc3af87a2596869a9
Carol Mariana Velazquez <cavelasquez7@poligran.edu.co>
2019-10-10 23:45:24 -0500

    Implement people service

--- src/app/modules/list/list.component.ts
+++ src/app/modules/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonModel } from 'src/app/shared/models/person.model';
+import { SwapiService } from 'src/app/shared/services/swapi/swapi.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  people: PersonModel[];
+  param: string;
 
-  constructor() { }
+  constructor(
+    private swapiService: SwapiService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.param = params.get('id');
+    });
+    this.initPeople();
   }
 
+  initPeople() {
+    this.swapiService.getPeople().subscribe(resolve => {
+      this.people = resolve.results;
+      console.log(resolve);
+    });
+  }
 }
