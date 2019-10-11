fb20165e97f9d6912ba36c9b02664329fcabb603
lucassanjuan <lusanjuan@poligran.edu.co>
2019-10-11 00:07:17 -0500

    Planet service implementation
    Se implemento el servicio de la consulta de los planetas

--- src/app/modules/list/list.component.ts
+++ src/app/modules/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PersonModel } from 'src/app/shared/models/person.model';
 import { SwapiService } from 'src/app/shared/services/swapi/swapi.service';
 import { ActivatedRoute } from '@angular/router';
+import { PlanetModel } from 'src/app/shared/models/planet.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListComponent implements OnInit {
   people: PersonModel[];
+  planets:PlanetModel[];
   param: string;
 
   constructor(
@@ -22,12 +24,20 @@ export class ListComponent implements OnInit {
       this.param = params.get('id');
     });
     this.initPeople();
+    this.initPlanets();
   }
 
   initPeople() {
     this.swapiService.getPeople().subscribe(resolve => {
       this.people = resolve.results;
       console.log(resolve);
+    });
+  }
+  initPlanets() {
+    this.swapiService.getPlanets().subscribe(resolve => {
+      this.planets = resolve.results;
+      console.log(resolve);
+      console.log("planetas");
     });
   }
 }
