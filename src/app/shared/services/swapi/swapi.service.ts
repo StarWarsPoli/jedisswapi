4e267a1b7568b54b8c24943dda5896c0c7a99000
Carol Mariana Velazquez <cavelasquez7@poligran.edu.co>
2019-10-10 23:45:09 -0500

    Create people service

--- src/app/shared/services/swapi/swapi.service.ts
+++ src/app/shared/services/swapi/swapi.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs'
-import { HttpClient } from '@angular/common/http'
-import { take } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
+  private urlBase = 'https://swapi.co/api/';
 
-  private urlBase = 'https://swapi.co/api/'
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllFilms(): Observable<any> {
-    const url = `${this.urlBase}films`
-    return this.http.get<any>(url).pipe(take(1))
+    const url = `${this.urlBase}films`;
+    return this.http.get<any>(url).pipe(take(1));
+  }
+
+  getPeople(): Observable<any> {
+    const url = `${this.urlBase}people`;
+    return this.http.get<any>(url).pipe(take(1));
   }
 }
